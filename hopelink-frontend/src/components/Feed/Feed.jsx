3f3106a97b73a91466be633852a7a4788e2a69e9
om kathar <omkathar15@gmail.com>
2024-11-08 20:21:19 +0530

    feed changed

--- hopelink-frontend/src/components/Feed/Feed.jsx
+++ hopelink-frontend/src/components/Feed/Feed.jsx
@@ -5,21 +5,6 @@ import axios from "axios";
 
 
 function Feed() {
-  // const [posts, setPosts] = useState(samplePosts);
-
-  // const toggleLike = (postId) => {
-  //   setPosts(
-  //     posts.map((post) =>
-  //       post.id === postId
-  //         ? {
-  //             ...post,
-  //             likes: post.liked ? post.likes - 1 : post.likes + 1,
-  //             liked: !post.liked,
-  //           }
-  //         : post
-  //     )
-  //   );
-  // };
   const [data, setData] = useState([]);
   useEffect(() => {
   axios.get("http://localhost:3000/feed")
@@ -38,7 +23,7 @@ function Feed() {
           <div className="post-header">
             <div className="user-info">
               <h4>{post.postTitle}</h4>
-              <p>{post.userTitle}</p>
+              <p>{post.userId}</p>
             </div>
           </div>
           <div className="post-content">
@@ -48,13 +33,7 @@ function Feed() {
               {post.postTags}
             </div>
           </div>
-          {/* <div className="post-actions">
-            <button onClick={() => toggleLike(post.id)}>
-              {post.liked ? "Unlike" : "Like"} ({post.likes})
-            </button>
-            <button>Comment ({post.comments})</button>
-            <button>Share</button>
-          </div> */}
+          
         </div>
       ))}
     </div>
